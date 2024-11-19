93d697462707467f1f660720e7fcd8d55fa41add
Robert Masen <r.f.masen@gmail.com>
2024-11-18 23:59:57 -0600

    ignore empty items

--- examples/scan_more.rs
+++ examples/scan_more.rs
@@ -1,9 +1,9 @@
 use cole_mine::discover;
+use futures::StreamExt;
 use std::{
     collections::{BTreeMap, BTreeSet},
     time::Duration,
 };
-use futures::StreamExt;
 use tokio::time::timeout;
 
 #[tokio::main]
@@ -15,14 +15,11 @@ async fn main() {
     let mut unknown_ct = 0;
     let mut stream = discover(true).await.unwrap();
     while let Some(dev) = stream.next().await {
-        println!("found device {}", dev.address());
         let name = dev.local_name().await.unwrap_or_else(|| {
             unknown_ct += 1;
             format!("dev-{unknown_ct}")
         });
-        println!("  name: {name}");
         let rssi = dev.rssi().await.unwrap_or_default();
-        println!("  rssi: {rssi}");
         let service_count = if let Ok(Ok(srv_ct)) =
             timeout(Duration::from_secs(max_op_secs), dev.service_count()).await
         {
@@ -30,7 +27,6 @@ async fn main() {
         } else {
             0
         };
-        println!("  with srv_ct: {service_count}");
         let characteristics: BTreeSet<String> =
             timeout(Duration::from_secs(1), dev.characteristics())
                 .await
@@ -39,22 +35,42 @@ async fn main() {
                 .into_iter()
                 .map(|c| c.uuid().to_string())
                 .collect();
-        println!("  char_ct: {}", characteristics.len());
-        println!("  chars:");
-        for ch in characteristics {
-            println!("    {ch}");
-        }
-        println!("  srv_ct: {service_count}");
+
+        let mut srvs = BTreeMap::new();
         if let Ok(Ok(services)) = timeout(Duration::from_secs(max_op_secs), dev.services()).await {
-            println!("  srvs:");
+            // println!("  srvs:");
             for s in services {
-                println!("    id: {}", s.uuid().as_simple());
-                let value = s
+                let key = s.uuid().as_simple().to_string();
+                let value: BTreeSet<String> = s
                     .characteristics()
                     .into_iter()
-                    .map(|c| c.uuid().as_simple().to_string());
-                for ch in value {
-                    println!("      {ch}")
+                    .map(|c| c.uuid().as_simple().to_string())
+                    .collect();
+                srvs.insert(key, value);
+            }
+        }
+        if !characteristics.is_empty()
+            || (!srvs.is_empty()
+            && srvs.iter().any(|(_, cs)| !cs.is_empty()))
+        {
+            println!("found device {}", dev.address());
+            println!("  name: {name}");
+            println!("  rssi: {rssi}");
+            println!("  char_ct: {}", characteristics.len());
+            if !characteristics.is_empty() {
+                println!("  chars:");
+            }
+            for ch in characteristics {
+                println!("    {ch}");
+            }
+            println!("  srv_ct: {service_count}");
+            if !srvs.is_empty() {
+                println!("  srvs:");
+                for (id, charas) in &srvs {
+                    println!("    srv: {id}");
+                    for ch in charas.iter() {
+                        println!("      ch: {ch}");
+                    }
                 }
             }
         }
